9861547e793860857718056edea7a393dd42d27e
Sineastra <lord.of.light.0002@gmail.com>
2021-03-07 00:23:07 +0200

    added 02 Github Repos

--- 02. Asynchronous Programming and Promises/Lab/02.Github-Repos/scripts/app.js
+++ 02. Asynchronous Programming and Promises/Lab/02.Github-Repos/scripts/app.js
@@ -1,3 +1,21 @@
-function loadRepos () {
-	console.log('TODO...')
+async function loadRepos () {
+	const html = {
+		nameField: document.getElementById(`username`),
+		resultE: document.getElementById(`repos`),
+	}
+
+	const data = await fetch(`https://api.github.com/users/${html.nameField.value}/repos`)
+	const deserilized = await data.json()
+
+	html.resultE.innerHTML = ''
+
+	deserilized.forEach(({ full_name, html_url }) => {
+		const li = document.createElement('li')
+		const a = document.createElement('a')
+		a.innerHTML = full_name
+		a.href = html_url
+
+		li.appendChild(a)
+		html.resultE.appendChild(li)
+	})
 }
