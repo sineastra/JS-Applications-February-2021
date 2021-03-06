fc48438b236b4754c7f15321b9483bbe3b4aaffe
Sineastra <lord.of.light.0002@gmail.com>
2021-03-07 01:09:16 +0200

    added 03 Github Commits

--- 02. Asynchronous Programming and Promises/Lab/03.Gighub-Commits/app.js
+++ 02. Asynchronous Programming and Promises/Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,33 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits () {
+	const html = {
+		nameField: document.getElementById(`username`),
+		repoField: document.getElementById(`repo`),
+		resultE: document.getElementById(`commits`),
+	}
+
+	html.resultE.innerHTML = ''
+
+	const eFactory = (tag, content = '') => {
+		const e = document.createElement(tag)
+		e.innerHTML = content
+
+		return e
+	}
+
+	try {
+		const data = await fetch(`https://api.github.com/repos/${html.nameField.value}/${html.repoField.value}/commits`)
+
+		if (! data.ok) throw new Error(`${data.status} (${data.statusText})`)
+
+		const deserialized = await data.json()
+
+		deserialized.forEach(({ commit }) => html.resultE.appendChild(eFactory(
+			'li',
+			`${commit.author.name}: ${commit.message}`
+		)))
+
+	} catch (e) {
+		html.resultE.appendChild(eFactory('li', e))
+	}
+
 }
