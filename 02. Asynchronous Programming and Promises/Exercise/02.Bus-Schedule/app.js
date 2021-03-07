740f387b3231858a15eed399dacb490121d03621
Sineastra <lord.of.light.0002@gmail.com>
2021-03-07 16:12:27 +0200

    added 02 Bus Schedule

--- 02. Asynchronous Programming and Promises/Exercise/02.Bus-Schedule/app.js
+++ 02. Asynchronous Programming and Promises/Exercise/02.Bus-Schedule/app.js
@@ -1,17 +1,44 @@
-function solve() {
+function solve () {
+	const html = {
+		info: document.getElementById(`info`),
+		depart: document.getElementById(`depart`),
+		arrive: document.getElementById(`arrive`),
+	}
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+	const getStop = async (name) => {
+		try {
+			const stop = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${name}`)
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+			return await stop.json()
+		} catch (e) {
+			html.info.innerHTML = 'Error'
+			html.arrive.disabled = true
+			html.depart.disabled = true
+		}
+	}
+	let nextStop
+	let nextStopName = 'depot'
 
-    return {
-        depart,
-        arrive
-    };
+
+	async function depart () {
+		html.depart.disabled = true
+		html.arrive.disabled = false
+		nextStop = await getStop(nextStopName)
+		html.info.innerHTML = `Next stop ${nextStop.name}`
+	}
+
+	function arrive () {
+		html.depart.disabled = false
+		html.arrive.disabled = true
+
+		html.info.innerHTML = `Arriving at ${nextStop.name}`
+		nextStopName = nextStop.next
+	}
+
+	return {
+		depart,
+		arrive
+	}
 }
 
-let result = solve();
+let result = solve()
